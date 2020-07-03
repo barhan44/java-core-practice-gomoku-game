0c48c59b8eb4a2f615deccbe0c2e736c829f20c1
barhan44 <gunyashov44@gmail.com>
2020-07-03 22:47:32 +0300

    init commit

---
+++ src/main/java/io/barhan/gomoku_game/App.java
@@ -0,0 +1,13 @@
+package io.barhan.gomoku_game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
